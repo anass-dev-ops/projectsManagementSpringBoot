b3041c19e40faf01f661dca52e804366ee0cfd72
anass <anass@DESKTOP-K0UPD1P>
2020-10-27 13:03:42 +0100

    Service of project enetity

--- ProjectsManagement/src/main/java/org/taskManagement/services/IProjectService.java
+++ ProjectsManagement/src/main/java/org/taskManagement/services/IProjectService.java
@@ -1,5 +1,12 @@
 package org.taskManagement.services;
 
-public interface IProjectService {
+import java.util.List;
 
+import org.taskManagement.entities.Project;
+
+public interface IProjectService {
+	public Project createProject(Project project);
+	public Project getProjectById(Long idProject);
+	public List<Project> getProjects();
+	public boolean deleteProject(Long idProject);
 }
--- ProjectsManagement/src/main/java/org/taskManagement/servicesImp/ProjectServiceImp.java
+++ ProjectsManagement/src/main/java/org/taskManagement/servicesImp/ProjectServiceImp.java
@@ -1,9 +1,39 @@
 package org.taskManagement.servicesImp;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.taskManagement.dao.ProjectRepository;
+import org.taskManagement.entities.Project;
 import org.taskManagement.services.IProjectService;
 
 @Service
 public class ProjectServiceImp implements IProjectService{
+	@Autowired
+	private ProjectRepository projectRepository;
+
+	@Override
+	public Project createProject(Project project) {
+		return projectRepository.save(project);
+	}
+
+	@Override
+	public Project getProjectById(Long idProject) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Project> getProjects() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteProject(Long idProject) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
